c76cda9582e9caea3a97533d80987ee15231d3e2
神代綺凜 <i@lolico.moe>
2019-01-16 02:42:26 +0800

    Abandon nhentai api

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   nhentai 下载增强
 // @name:zh-TW   nhentai 下載增強
 // @namespace    https://github.com/Tsuk1ko
-// @version      1.0.1
+// @version      1.1.0
 // @icon         https://nhentai.net/favicon.ico
 // @description        Add a "download zip" button for nhentai gallery page
 // @description:zh-CN  为 nhentai 本子页增加 zip 打包下载
@@ -13,10 +13,10 @@
 // @connect      i.nhentai.net
 // @license      GPL-3.0
 // @grant        GM_addStyle
-// @require      https://code.jquery.com/jquery-2.2.4.min.js
+// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // @require      https://cdn.bootcss.com/jszip/3.1.4/jszip.min.js
 // @require      https://cdn.bootcss.com/FileSaver.js/1.3.2/FileSaver.min.js
-// @require      https://unpkg.com/axios/dist/axios.min.js
+// @require      https://cdn.bootcss.com/axios/0.18.0/axios.min.js
 // @run-at       document-end
 // @noframes
 // @homepageURL  https://github.com/Tsuk1ko/nhentai-download-as-zip
@@ -27,12 +27,7 @@
 
 (async function () {
 	'use strict';
-	const THREAD = 5;
-	const EXT = {
-		j: 'jpg',
-		p: 'png',
-		g: 'gif'
-	}
+	const THREAD = 8;
 	const MIME = {
 		png: "image/png",
 		jpg: "image/jpeg",
@@ -40,7 +35,7 @@
 	};
 
 	function mimeType(suffix) {
-        if (!MIME[suffix]) throw new Error(`Unknown suffix ${suffix}`);
+		if (!MIME[suffix]) throw new Error(`Unknown suffix ${suffix}`);
 		return MIME[suffix];
 	};
 
@@ -60,36 +55,11 @@
 
 	//获取本子信息
 	btnState(false);
-	let gid = /^https:\/\/nhentai\.net\/g\/([0-9]+)\/$/.exec(window.location.href)[1];
-	let {
-		mid,
-		pages,
-		title
-	} = await axios.get(`https://nhentai.net/api/gallery/${gid}`).then(ret => {
-		let {
-			images: {
-				pages
-			},
-			media_id,
-			title: {
-				english,
-				japanese
-			}
-		} = ret.data;
-		let title = japanese || english;
-
-		let parsePages = [];
-		for (let i = 0; i < pages.length; i++) {
-			let t = pages[i].t;
-			if (!EXT[t]) throw new Error(`Unknown page type ${t}`);
-			parsePages.push([i + 1, EXT[t]]);
-		}
-
-		return {
-			mid: media_id,
-			pages: parsePages,
-			title: title.replace(/[/\\:*?"<>|.&$ ]+/g, ' ')
-		};
+	let title = $('#info > h2').html() || $('#info > h1').html();
+	title = title.replace(/[/\\:*?"<>|.&$ ]+/g, ' ');
+	let pages = [];
+	$('#thumbnail-container > .thumb-container img.lazyload').each(function () {
+		pages.push($(this).attr('data-src').replace('t.nhentai.net', 'i.nhentai.net').replace('t.', '.'));
 	});
 	btnState(true);
 	btnTxt(`Download as zip 0/${pages.length}`);
@@ -117,16 +87,16 @@
 	let zipBlob = false;
 	let done = 0;
 
-	function pagePromise(threadID, page) {
-		let url = `https://i.nhentai.net/galleries/${mid}/${page[0]}.${page[1]}`;
+	function pagePromise(threadID, url) {
+		let page = /([^\/]+)\.([^.]+)$/.exec(url);
 		console.log(`[${threadID}] Downloading ${url}`);
 		return axios.get(url, {
 			responseType: 'arraybuffer'
 		}).then(ret => {
 			let blob = new Blob([ret.data], {
-				type: mimeType(page[1])
+				type: mimeType(page[2])
 			});
-			zip.file(`${page[0]}.${page[1]}`, blob, {
+			zip.file(`${page[1]}.${page[2]}`, blob, {
 				binary: true
 			});
 			btnTxt(`Downloading ${++done}/${pages.length}`);
