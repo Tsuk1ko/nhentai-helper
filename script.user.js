09224f772b3a5fd73e18c070405ad2837d627efa
神代綺凜 <i@loli.best>
2020-03-12 20:40:47 +0800

    v2.5.1 NyaHentai 下载原图（忘了加了）

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   nHentai 助手
 // @name:zh-TW   nHentai 助手
 // @namespace    https://github.com/Tsuk1ko
-// @version      2.5.0
+// @version      2.5.1
 // @icon         https://nhentai.net/favicon.ico
 // @description        Download nHentai doujin as compression file easily, and add some useful features. Also support NyaHentai.
 // @description:zh-CN  为 nHentai 增加压缩打包下载方式以及一些辅助功能，同时支持 NyaHentai
@@ -259,7 +259,7 @@
         const url = `https://nhentai.net/api/gallery/${gid}`;
         return isNyahentai ? proxyGetJSON(url) : get(url);
     };
-    const getDownloadURL = (mid, filename) => (isNyahentai ? `https://search.pstatic.net/common?src=https://i.nyahentai.net/galleries/${mid}/${filename}` : `https://i.nhentai.net/galleries/${mid}/${filename}`);
+    const getDownloadURL = (mid, filename) => (isNyahentai ? `https://search.pstatic.net/common?type=origin&src=https://i.nyahentai.net/galleries/${mid}/${filename}` : `https://i.nhentai.net/galleries/${mid}/${filename}`);
 
     // 伪多线程
     const multiThread = (tasks, promiseFunc) => {
