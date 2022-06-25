a1a2b7e82a923454bb8f677c1c2edfc101a01362
神代綺凛 <i@loli.best>
2022-06-25 17:52:52 +0800

    feat: change CDN

--- jszip.worker.js
+++ jszip.worker.js
@@ -1,6 +1,6 @@
 importScripts(
-  'https://code.bdstatic.com/npm/comlink@4.3.1/dist/umd/comlink.min.js',
-  'https://code.bdstatic.com/npm/jszip@3.7.1/dist/jszip.min.js'
+  'https://fastly.jsdelivr.net/npm/comlink@4.3.1/dist/umd/comlink.min.js',
+  'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/jszip/3.7.1/jszip.min.js'
 );
 
 class JSZipWorker {
@@ -11,9 +11,7 @@ class JSZipWorker {
     this.zip.file(name, data);
   }
   generateAsync(options, onUpdate) {
-    return this.zip
-      .generateAsync(options, onUpdate)
-      .then(data => Comlink.transfer({ data }, [data]));
+    return this.zip.generateAsync(options, onUpdate).then(data => Comlink.transfer({ data }, [data]));
   }
 }
 
