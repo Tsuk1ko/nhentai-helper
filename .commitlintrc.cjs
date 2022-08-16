1d76e0062d8c73f6b6f5641725a4dd49a73b40ae
神代綺凛 <i@loli.best>
2022-08-17 00:20:07 +0800

    feat: config type

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 };
--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
   env: {
     browser: true,
