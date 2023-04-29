4bb4520eaff610d3084f84838c886c0dd32ab52b
神代綺凛 <i@loli.best>
2023-04-29 23:15:00 +0800

    feat: only hack Date on nHentai

--- src/utils/hacks/date.ts
+++ src/utils/hacks/date.ts
@@ -1,8 +1,8 @@
 import { monkeyWindow, unsafeWindow } from '$';
 import logger from '../logger';
-import { IS_DEV } from '@/const';
+import { IS_DEV, IS_NHENTAI } from '@/const';
 
-if (IS_DEV) {
+if (IS_DEV && IS_NHENTAI) {
   logger.log('fix window.Date');
   unsafeWindow.Date = monkeyWindow.Date;
 }
