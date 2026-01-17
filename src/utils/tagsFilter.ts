f281b7cf1273353405850d21fa9fa1a0517bcf03
神代綺凛 <i@loli.best>
2026-01-17 12:14:58 +0800

    feat: tags filter for xxx and to

--- src/utils/tagsFilter.ts
+++ src/utils/tagsFilter.ts
@@ -31,6 +31,10 @@ export const filterTagsMap: Record<string, FilterTag> = IS_NHENTAI_TO
         type: FilterTagType.LANGUAGE,
         value: '10197',
       },
+      uncensored: {
+        type: FilterTagType.OTHER,
+        value: '632',
+      },
     }
   : IS_NHENTAI_XXX
     ? {
@@ -45,6 +49,10 @@ export const filterTagsMap: Record<string, FilterTag> = IS_NHENTAI_TO
         chinese: {
           type: FilterTagType.LANGUAGE,
           value: '3',
+        },
+        uncensored: {
+          type: FilterTagType.OTHER,
+          value: '18',
         },
       }
     : {
@@ -80,23 +88,25 @@ const langDetectRegMap = Object.entries({
   chinese: /\[chinese\]|中国翻訳|漢化\]/i,
 });
 
+const TAG_ATTR_NAME = 'data-tags';
+const LANGUAGE_DATA_NAME = IS_NHENTAI_XXX ? 'languages' : 'tags';
+const LANGUAGE_ATTR_NAME = `data-${LANGUAGE_DATA_NAME}`;
+const HIDDEN_CLASS = 'nhentai-helper-hidden';
+
 const handleMissingDataTags = ($gallery: JQElement) => {
   $gallery.each(function () {
     const title = $(this).find(selector.galleryCaption).text();
     const lang = langDetectRegMap.find(([, reg]) => reg.test(title))?.[0] || 'japanese';
     const tag = filterTagsMap[lang];
     if (tag) {
-      const curTags = this.dataset.tags;
-      this.dataset.tags = `${curTags ? `${curTags} ` : ''}${tag.value}`;
+      const curTags = this.dataset[LANGUAGE_DATA_NAME];
+      this.dataset[LANGUAGE_DATA_NAME] = `${curTags ? `${curTags} ` : ''}${tag.value}`;
     }
   });
 };
 
-const TAG_ATTR_NAME = IS_NHENTAI_XXX ? 'data-languages' : 'data-tags';
-const HIDDEN_CLASS = 'nhentai-helper-hidden';
-
-const getNotTagSelector = (items: FilterTag[]) =>
-  items.map(item => `:not([${TAG_ATTR_NAME}~=${item.value}])`).join('');
+const getNotTagSelector = (items: FilterTag[], attrName = TAG_ATTR_NAME) =>
+  items.map(item => `:not([${attrName}~=${item.value}])`).join('');
 
 /** 语言过滤 */
 export const doFilterTags = (tags: FilterTag[], $node?: JQElement): void => {
@@ -105,7 +115,9 @@ export const doFilterTags = (tags: FilterTag[], $node?: JQElement): void => {
     : (selector: string) => $(selector);
 
   getNode(selector.gallery).removeClass(HIDDEN_CLASS);
-  handleMissingDataTags(getNode(`${selector.gallery}${getNotTagSelector(allLangTags)}`));
+  handleMissingDataTags(
+    getNode(`${selector.gallery}${getNotTagSelector(allLangTags, LANGUAGE_ATTR_NAME)}`),
+  );
 
   const { [FilterTagType.LANGUAGE]: langTags, [FilterTagType.OTHER]: otherTags } = groupBy(
     tags,
@@ -113,7 +125,9 @@ export const doFilterTags = (tags: FilterTag[], $node?: JQElement): void => {
   );
 
   if (langTags?.length) {
-    getNode(`${selector.gallery}${getNotTagSelector(tags)}`).addClass(HIDDEN_CLASS);
+    getNode(`${selector.gallery}${getNotTagSelector(tags, LANGUAGE_ATTR_NAME)}`).addClass(
+      HIDDEN_CLASS,
+    );
   }
 
   otherTags?.forEach(tag => {
